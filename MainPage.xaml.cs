17c8b8abd367aca7be5072ab360d0bee4c469aea
alex110302 <hansenaalexander@gmail.com>
2024-09-20 13:36:39 -0700

    first push of the timer app I made

---
+++ MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Maui.Layouts;
+using TimerPro.Custom;
+
+namespace TimerPro;
+
+public partial class MainPage : ContentPage
+{
+    TimerLogic oTimer = new TimerLogic();
+
+    bool isRunning = false;
+    
+    public MainPage()
+    {
+        InitializeComponent();
+        Title = "Timer Pro";
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        btnStart.IsEnabled = true;
+        btnStop.IsEnabled = false;
+    }
+
+    private void BtnStart_OnClicked(object sender, EventArgs e)
+    {
+        isRunning = true;
+        btnStart.IsEnabled = false;
+        btnStop.IsEnabled = true;
+        
+        Application.Current.Dispatcher.StartTimer(TimeSpan.FromSeconds(1), () =>
+        {
+            if (isRunning)
+            {
+                oTimer.SetTickCount();
+                lblDisplay.Text = oTimer.GetFromatedString();
+            }
+            
+            return isRunning;
+        });
+    }
+
+    private void BtnStop_OnClicked(object sender, EventArgs e)
+    {
+        isRunning = false;
+        btnStart.IsEnabled = true;
+        btnStop.IsEnabled = false;
+    }
+
+    private void btnReset_OnClicked(object sender, EventArgs e)
+    {
+        oTimer.Reset();
+        lblDisplay.Text = oTimer.GetFromatedString();
+    }
+}
